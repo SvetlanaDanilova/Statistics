39ffd712ee3faa4020db1f83afb7e1368c61bf68
SvetlanaDanilova <90624619+SvetlanaDanilova@users.noreply.github.com>
2023-06-03 21:06:39 +0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -36,7 +36,7 @@ if uploaded_file is not None:
 
   st.pyplot(fig)
   
-  number_of_days = np.max(df['work_days'])
+  number_of_days = max(df['work_days'])
   days = st.slider('Задайте количество дней n в гипотезе', 0, number_of_days, number_of_days)
   
   df['more_n_days'] = np.where(df['work_days'] > days, 1, 0)
