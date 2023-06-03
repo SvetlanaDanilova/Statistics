d7b5aac0cfa1eba084e792179b301cfa12178bf2
SvetlanaDanilova <90624619+SvetlanaDanilova@users.noreply.github.com>
2023-06-03 20:08:44 +0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,11 +1,12 @@
+import streamlit as st
+
 import numpy as np
 import pandas as pd
-
-import streamlit as st
+import plotly.figure_factory as ff
 
 uploaded_file = st.file_uploader('Выберите файл для анализа данных с расширением csv')
 
-df = pd.read_csv('data.csv', sep=',', encoding='cp1251')  
+df = pd.read_csv(uploaded_file, sep=',', encoding='cp1251')  
 df.rename(columns={'Количество больничных дней': 'work_days', 'Возраст': 'age', 'Пол' : 'sex'}, inplace=True)
 df['sex'].replace(['М', 'Ж'], [0, 1], inplace=True)
 
