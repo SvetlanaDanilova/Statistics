021eed7ec33bb795b0fda563ab18e73f7d75affc
SvetlanaDanilova <90624619+SvetlanaDanilova@users.noreply.github.com>
2023-06-03 20:06:05 +0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 import streamlit as st
 
+uploaded_file = st.file_uploader('Выберите файл для анализа данных с расширением csv')
+
 df = pd.read_csv('data.csv', sep=',', encoding='cp1251')  
 df.rename(columns={'Количество больничных дней': 'work_days', 'Возраст': 'age', 'Пол' : 'sex'}, inplace=True)
 df['sex'].replace(['М', 'Ж'], [0, 1], inplace=True)
@@ -11,4 +13,4 @@ male = df[df['sex'] == 0]['work_days']
 female = df[df['sex'] == 1]['work_days']
 
 st.title('Проверка гипотез')
-st.markdown('Гипотеза 1: Мужчины пропускают в течение года более 2 рабочих дней по болезни значимо чаще женщин.')
+st.markdown('Гипотеза 1: Мужчины пропускают в течение года более 2 рабочих дней по болезни значимо чаще женщин')
