b1d7be944d459e823394562d22b02758021d6379
SvetlanaDanilova <90624619+SvetlanaDanilova@users.noreply.github.com>
2023-06-03 20:14:48 +0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -2,7 +2,6 @@ import streamlit as st
 
 import numpy as np
 import pandas as pd
-import plotly.figure_factory as ff
 import matplotlib.pyplot as plt
 
 uploaded_file = st.file_uploader('Выберите файл для анализа данных с расширением csv')
