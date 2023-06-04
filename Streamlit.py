1cbea2cf0a4a3ad9aa3ac48726c19541ed000528
SvetlanaDanilova <90624619+SvetlanaDanilova@users.noreply.github.com>
2023-06-04 19:07:13 +0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -34,8 +34,10 @@ def print_answer(answer, type):
             st.write("Принимаем альтернативную гипотезу о том, что люди постарше пропускают рабочие дни чаще")       
 
 def draw_hist(array1, label1, array2, label2):
-    bin_size = 1; min_edge = 0; max_edge = 8
-    N = (max_edge-min_edge)/bin_size
+    bin_size = 1
+    min_edge = 0
+    max_edge = 8
+    N = (max_edge - min_edge) / bin_size
     Nplus1 = N + 1
     bin_list = np.linspace(min_edge, max_edge, Nplus1)
 
